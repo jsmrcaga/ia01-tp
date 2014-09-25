555895977cf4de645e7a001ff6c83676931b53d8
Mewen <mewenmichel@yahoo.fr>
2014-09-25 17:15:36 +0200

    Déjà fait + sujet du tp

---
+++ rendu.lisp
@@ -0,0 +1,23 @@
+(defun reverseA (arg1 arg2 arg3) ()
+	(list arg3 arg2 arg1)
+)
+
+(defun reverseB (L)
+	(cond
+		((= (length L) 1) L)
+		((= (length L) 2) (list (cadr L) (car L)))
+		((= (length L) 3) (list (caddr L) (cadr L) (car L)))
+	)
+)
+
+(defun reverseC (L)( ; attention ! ya une erreur !
+	if (> (length L) 1) ; condition
+		(list (reverseC (cdr L)) (car L)) ; then
+		(car L) ; else
+	)
+)
+
+(defun double(L)(
+	
+	)
+)

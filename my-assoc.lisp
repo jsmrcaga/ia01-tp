4cb185c8ea0f288bea3b15b6b6baab47cdd5316e
Jo Colina <jo@jocolina.com>
2014-09-28 17:28:49 +0200

    My Assoc Debut

---
+++ my-assoc.lisp
@@ -0,0 +1,15 @@
+; Check le premier element de chaque duplet dans a-liste
+; Si la cle est la meme, le retour de la fonction est le
+; duplet lui meme, permettant ainsi de parcourir une base
+; de donnees en duplet
+
+(defun my-assoc(cle liste)
+     (mapcar
+      '(lambda (l)
+                (if (eq (car l) cle)
+				(print l);then
+				(NIL);else
+			))
+		liste
+	)
+)

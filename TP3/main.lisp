9a61d0dd55cc605171892b38af1a5e713d341da7
Jo Colina <jo@jocolina.com>
2014-12-21 17:22:01 +0100

    First Commit TP3

---
+++ TP3/main.lisp
@@ -0,0 +1,48 @@
+;Template des regles
+("Nom de la regle" ((qtteElement element) (qtteElement2 element2) (qtteElement3 element3)) "resultat")
+
+;Recursivite sur les elements pour trouver les profondeurs des recettes
+; + qtte des elements pour faire le 0+ du moteur
+;Appareil => on ajoute des melanges recursifs pour les recettes en tant que mini recettes.
+
+
+(defun getRecipe (listOfRecipes nameRecipe) (
+
+	(assoc nameRecipe listOfRecipes)
+
+	)
+)
+
+(defun verifyFacts () (
+
+
+	))
+
+(defun getFirstIngredients () (
+
+	(print "Veuillez entrer les ingredients que vous possedez")
+
+
+	)
+
+)
+
+(defun printIngred (lettre)(
+
+
+
+
+	)
+)
+
+(defun printExplanation () (
+
+	(print "Bienvenue au Systeme Expert de Cuisine!")
+	(print "Grace a ce systeme vous pourrez trouver des recettes a partir des ingredients que vous possedez et du temps que vous comptez consacrer a la preparation de la recette")
+	(print "Veuillez donc nous indiquer les ingredients que vous voulez choisir en commencant par taper la premiere lettre du nom")
+	(print "On vous proposera donc une liste d'ingredients, il suffira de taper le numero de lingredient correspondant")
+	(print "On vous demandera ensuite la qtte que vous en possedez, liquides en ml, solides en grammes, et autres en unites")
+
+	)
+
+)

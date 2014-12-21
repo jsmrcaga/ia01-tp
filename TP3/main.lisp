8716457302e16dd0f3a976c17a26e2b77d9a5ec9
Jo Colina <jo@jocolina.com>
2014-12-21 17:51:44 +0100

    Plep

--- TP3/main.lisp
+++ TP3/main.lisp
@@ -5,37 +5,43 @@
 ; + qtte des elements pour faire le 0+ du moteur
 ;Appareil => on ajoute des melanges recursifs pour les recettes en tant que mini recettes.
 
-
-(defun getRecipe (listOfRecipes nameRecipe) (
+;Template listOfIngredients
+(defun getRecipe (listOfRecipes nameRecipe) 
 
 	(assoc nameRecipe listOfRecipes)
 
-	)
+	
 )
 
-(defun verifyFacts () (
+(defun verifyFacts () 
 
 
-	))
+	)
 
-(defun getFirstIngredients () (
+(defun getFirstIngredients () 
 
 	(print "Veuillez entrer les ingredients que vous possedez")
 
 
-	)
+	
 
 )
 
-(defun printIngred (lettre)(
+(defun printIngred (lettre)
 
+	(dolist (current listOfRecipes)
 
+		(dolist (currentIngredient (cadr current))
 
+			(princ (cadr currentIngredient))
 
-	)
+			)
+
+		)
+	
 )
 
-(defun printExplanation () (
+(defun printExplanation () 
 
 	(print "Bienvenue au Systeme Expert de Cuisine!")
 	(print "Grace a ce systeme vous pourrez trouver des recettes a partir des ingredients que vous possedez et du temps que vous comptez consacrer a la preparation de la recette")
@@ -43,6 +49,7 @@
 	(print "On vous proposera donc une liste d'ingredients, il suffira de taper le numero de lingredient correspondant")
 	(print "On vous demandera ensuite la qtte que vous en possedez, liquides en ml, solides en grammes, et autres en unites")
 
-	)
+	
 
 )
+
